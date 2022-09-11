31b8b055288fe9684497576dd3ceb4251eb34305
deepenmhrj <deepenmhrj@gmail.com>
2022-09-11 12:43:04 -0700

    post api

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
 const tableElement = document.querySelector('table');
 
-const URL = `http://localhost:3000`
+const URL = `http://localhost:3000/employees`;
+
+const postData = () => {
+  const title = document.getElementById("title").value;
+  const current = document.getElementById("current").value;
+  const previous = document.getElementById("previous").value;
+  // console.log(title, current, previous);
+
+  const inputData = { title, current, previous };
+  const method = "POST";
+
+
+  fetch(URL, {
+    method,
+    body: JSON.stringify(inputData),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    if (res.status = 201) {
+      console.log('Employee Added!')
+    }
+  })
+}
+
 const getData = async () => {
   try {
-    const resp = await fetch(URL + '/employees');
+    const resp = await fetch(URL);
     let data = await resp.json();
     data = JSON.stringify(data);
     data = JSON.parse(data);
 
     let rows = `
       <tr>
-          <th><input class="form-control" placeholder = "ID" id ='id'></th>
-          <th><input class="form-control" placeholder = "Designation" id = "title"></th>
-          <th><input class="form-control" placeholder = "Current Employer" id = "current"></th>
-          <th><input class="form-control" placeholder = "Past Employer" id = "title"></th>
-          <td>
-            <button type="button" class="btn btn-success">Add</button>
-          </td>
+        <form action=${URL}>
+        <th>ID</th>
+        <th><input type = "text" name = "title" class="form-control" placeholder = "Designation" id = "title"></th>
+        <th><input type = "text" name = "current" class="form-control" placeholder = "Current Employer" id = "current"></th>
+        <th><input type = "text" name = "previous" class="form-control" placeholder = "Past Employer" id = "previous"></th>
+        <td>
+          <input type="submit" class="btn btn-success" value = "Add" onclick = "postData()"></input>
+        </td>
+      </form>
       </tr>`;
+
     data.forEach(employee => {
       rows = rows + `
         <tr>
@@ -26,7 +54,7 @@ const getData = async () => {
           <td><input class="form-control" value = ${employee.current}></td>
           <td><input class="form-control" value = ${employee.past}></td>
           <td>
-            <button type="button" class="btn btn-primary">Remove</button>
+            <button type="button" class="btn btn-primary" >Remove</button>
           </td>
 
 
